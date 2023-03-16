9abba82b81a711f3b53a0b4c97a05d290027bea6
Colin Shaw <colin.shaw400@outlook.com>
2023-03-16 13:59:27 +0000

    Add post model and admin

--- secure_app/blog/models.py
+++ secure_app/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    blogger = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    # defining delf string to return the 
+    # title of the blog and blooger name
+    def __str__(self):
+        return self.title + '|' + str(self.blogger)
