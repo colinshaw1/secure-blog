f1148573650785d2fcbf9e74f609ce25894691ce
Colin Shaw <colin.shaw400@outlook.com>
2023-03-16 15:56:33 +0000

    add homepage url

--- secure_app/blog/urls.py
+++ secure_app/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import BlogView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # using class based views for homepage
+    path('', BlogView.as_view(), name="home")
 ]
