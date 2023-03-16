372ddfb6d9bde5df0f8dcde98e0967c4afa7dd3b
Colin Shaw <colin.shaw400@outlook.com>
2023-03-16 15:48:43 +0000

    Add homepage view

--- secure_app/blog/views.py
+++ secure_app/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DeleteView
 # Create your views here.
-# create home page view
-def home(request):
-    return render(request, 'homepage.html', {})
+# class for creating the home view
+class BlogView(ListView):
+    # calls the Post model 
+    model = Post
+    template_name = 'homepage.html'
