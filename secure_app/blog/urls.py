c89e86c26ba720b0dca36134569e6520d2cacf75
Colin Shaw <colin.shaw400@outlook.com>
2023-03-17 12:19:51 +0000

    add blog url

--- secure_app/blog/urls.py
+++ secure_app/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 # from . import views
-from .views import BlogView, DetailsPost
+from .views import BlogView, DetailsPost, AddBlog
 
 urlpatterns = [
     # using class based views for homepage
     path('', BlogView.as_view(), name="home"),
-     # using class based views for homepage
+    # using class based views to view a blog post
     path('blog/<int:pk>', DetailsPost.as_view(), name="blogpost"),
+    # add blog post view
+    path('add_post/', AddBlog.as_view(), name="add_blog"),
 ]
