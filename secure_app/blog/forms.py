357f17ca0e5a1481636060b6a614cef8a1781700
Colin Shaw <colin.shaw400@outlook.com>
2023-03-17 13:18:42 +0000

    add placholder text

--- secure_app/blog/forms.py
+++ secure_app/blog/forms.py
@@ -9,8 +9,8 @@ class PostForm(forms.ModelForm):
         # add a dictonary for form 
         widgets = {
             # form-control is used for css styling
-            'title': forms.TextInput(attrs={'class':'form-control'}),
-            'slug': forms.TextInput(attrs={'class':'form-control'}),
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Please enter a title for you blog'}),
+            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Please enter a blog tag'}),
             'blogger': forms.Select(attrs={'class':'form-control'}),
-            'body': forms.Textarea(attrs={'class':'form-control'}),
+            'body': forms.Textarea(attrs={'class':'form-control', 'placeholder': 'Please enter your blog post here :)'}),
         }
