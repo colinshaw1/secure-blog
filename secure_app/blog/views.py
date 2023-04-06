f99015f64fc8f97acbdca0a484cec9045d0ca56b
Colin Shaw <colin.shaw400@outlook.com>
2023-04-06 10:07:17 +0000

    add edit blog post html page and view

--- secure_app/blog/views.py
+++ secure_app/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView, CreateView
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView
 from .models import Post
 from .forms import PostForm
 # Create your views here.
@@ -20,3 +20,10 @@ class AddBlog(CreateView):
     # tell the app to use the post form
     form_class = PostForm
     template_name = 'add_blog.html'
+
+
+# class to edit a blog
+class EditBlog(UpdateView):
+    model = Post
+    template_name = "update_blog.html"
+    fields = ['title', 'slug', 'body']
