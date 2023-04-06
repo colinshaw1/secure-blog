b162367cc6645ffa3497cd62d9c6c1697b8bcd17
Colin Shaw <colin.shaw400@outlook.com>
2023-04-06 12:29:07 +0000

    add created model for ordering blog posts by date

--- secure_app/blog/models.py
+++ secure_app/blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from datetime import datetime, date
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
     blogger = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    created_on = models.DateField(auto_now_add=True)
     # defining delf string to return the 
     # title of the blog and blooger name
     def __str__(self):
